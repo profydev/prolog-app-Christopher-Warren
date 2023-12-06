90af3247cc330c0e53dbc31b5aed5dcb86981fa6
Christopher Warren <chrisalmith@gmail.com>
2023-12-06 09:08:40 -0500

    remove console log

--- api/version.ts
+++ api/version.ts
@@ -3,6 +3,5 @@ import type { Version } from "@api/version.types";
 
 export async function getVersion() {
   const { data } = await axios.get<Version>("/api/version", { baseURL: "/" });
-  console.log("data");
   return data;
 }
