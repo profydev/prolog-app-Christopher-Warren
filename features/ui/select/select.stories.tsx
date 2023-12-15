56112f98bf2c2355b8fcca570385527b54c3dfe8
Christopher Warren <chrisalmith@gmail.com>
2023-12-14 20:56:51 -0500

    UI kit select (#14)
    * add css shadow variables
    
    * cleanup footer
    
    * add Select component
    
    * add select story
    
    * add disabled state
    
    * update style rules

---
+++ features/ui/select/select.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { Select } from "@features/ui/";
+
+export default {
+  title: "UI/Select",
+  component: Select,
+  paramaters: {
+    layout: "fullscreen",
+  },
+} as Meta<typeof Select>;
+
+const Template: StoryFn<typeof Select> = ({
+  error,
+  hint,
+  label,
+  logoSrc,
+  optionsList,
+}) => (
+  <div style={{ maxWidth: "320px" }}>
+    <Select
+      optionsList={optionsList}
+      error={error}
+      hint={hint}
+      label={label}
+      logoSrc={logoSrc}
+    />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  optionsList: [
+    { value: "James woodruf", id: "123" },
+    { value: "Bob Mcdill", id: "123123" },
+    { value: "Feris Wilson", id: "1ewew" },
+  ],
+  hint: "I am a hint",
+  label: "Don't label me!",
+};
+Default.parameters = {
+  viewMode: "docs",
+};
---
+++ features/ui/select/select.tsx
@@ -0,0 +1,119 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, MouseEventHandler, useEffect, useRef } from "react";
+import classNames from "classnames";
+import styles from "./select.module.scss";
+
+type Option = {
+  id: string;
+  value: string;
+};
+
+type SelectProps = {
+  optionsList: Option[];
+  logoSrc?: string;
+  label?: string;
+  hint?: string;
+  error?: Error;
+  disabled?: boolean;
+};
+
+export function Select({
+  optionsList,
+  logoSrc,
+  label,
+  error,
+  hint,
+  disabled,
+}: SelectProps) {
+  const [toggleSelect, setToggleSelect] = useState(false);
+  const [selectedValue, setSelectedValue] = useState<Option | null>(null);
+
+  const selectRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleMouseDown = (e: MouseEvent) => {
+      if (disabled) return;
+      if (selectRef.current && !selectRef.current.contains(e.target as Node)) {
+        setToggleSelect(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleMouseDown);
+
+    return () => document.removeEventListener("mousedown", handleMouseDown);
+  });
+
+  const handleToggleSelect: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    if (disabled) return;
+    setToggleSelect(!toggleSelect);
+  };
+
+  const handleSelectValue = (option: Option) => {
+    if (disabled) return;
+    setSelectedValue(option);
+    setToggleSelect(false);
+  };
+
+  const renderOptions = () => {
+    return optionsList.map((option) => {
+      const isSelected = option.value === selectedValue?.value;
+      return (
+        <li key={option.id}>
+          <button
+            tabIndex={0}
+            className={classNames(isSelected && styles.isSelected)}
+            onClick={() => handleSelectValue(option)}
+          >
+            {option.value}
+            {isSelected && (
+              <img src="/icons/check-large.svg" alt="" aria-disabled />
+            )}
+          </button>
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div ref={selectRef} className={styles.mainContainer}>
+      <label>
+        <span className={styles.label}>{label}</span>
+        <button
+          disabled={disabled}
+          onClick={handleToggleSelect}
+          className={classNames(
+            styles.dropdown,
+
+            !selectedValue && styles.placeholder,
+            error && styles.hasError,
+          )}
+        >
+          <div className={styles.content}>
+            {logoSrc && (
+              <img src={logoSrc} className={styles.icon} alt="" aria-disabled />
+            )}
+            {selectedValue?.value || "Select team member"}
+          </div>
+
+          <img src="/icons/chevron-down.svg" alt="" aria-disabled />
+        </button>
+      </label>
+      {!error && !toggleSelect && hint && (
+        <span className={styles.hint}>{hint}</span>
+      )}
+      {!toggleSelect && error && (
+        <span className={styles.error}>{error.message}</span>
+      )}
+      <ul
+        className={classNames(
+          toggleSelect && styles.show,
+          !selectedValue && styles.placeholder,
+          styles.optionsContainer,
+        )}
+      >
+        {renderOptions()}
+      </ul>
+    </div>
+  );
+}
