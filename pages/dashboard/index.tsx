733354cfeabef3a719a3324ad4b1290039036554
Christopher Warren <chrisalmith@gmail.com>
2023-12-18 18:24:18 -0500

    remove select from dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,30 +1,13 @@
 import type { NextPage } from "next";
 import { PageContainer } from "@features/layout";
 import { ProjectList } from "@features/projects";
-import { Select } from "@features/ui";
-import { useState } from "react";
 
 const Home: NextPage = () => {
-  const [selectedValue, setSelectedValue] = useState<{
-    id: string;
-    value: string;
-  } | null>(null);
-
   return (
     <PageContainer
       title="Projects"
       info="Overview of your projects sorted by alert level."
     >
-      <Select
-        placeholder="do something"
-        selectedValue={selectedValue}
-        onChange={(option) => setSelectedValue(option)}
-        onReset={() => setSelectedValue(null)}
-        optionsList={[
-          { id: "bob", value: "bob" },
-          { id: "sally", value: "sally" },
-        ]}
-      />
       <ProjectList />
     </PageContainer>
   );
