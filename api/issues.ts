4f0720148e108b65c48d015eb261e867adcbbbe7
Christopher Warren <chrisalmith@gmail.com>
2023-12-21 22:01:41 -0500

    remove params from url when empty

--- api/issues.ts
+++ api/issues.ts
@@ -4,13 +4,25 @@ import type { Page } from "@typings/page.types";
 
 const ENDPOINT = "/issue";
 
+const removeEmptyParams = (params: IssuesParams) => {
+  Object.entries(params).forEach(([key, value]) => {
+    if (!value) {
+      delete params[key as keyof IssuesParams];
+    }
+  });
+
+  return params;
+};
+
 export async function getIssues(
   params: IssuesParams,
   options?: { signal?: AbortSignal },
 ) {
+  const parsedParams = removeEmptyParams(params);
   const { data } = await axios.get<Page<Issue>>(ENDPOINT, {
-    params,
+    params: parsedParams,
     signal: options?.signal,
   });
+
   return data;
 }
