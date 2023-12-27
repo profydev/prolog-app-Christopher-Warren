0b4a101495d0807fde8fb8cd8021295be2d28e35
Christopher Warren <chrisalmith@gmail.com>
2023-12-26 19:46:00 -0500

    add langeing-page-container

--- features/layout/landing-page-container/landing-page-container.tsx
+++ features/layout/landing-page-container/landing-page-container.tsx
@@ -1,21 +1,24 @@
 import { HorizontalNavigation } from "@features/layout/";
 import styles from "./landing-page-container.module.scss";
+import Head from "next/head";
 
-export function LandingPageContainer() {
+export function LandingPageContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className={styles.mainContainer}>
+    <div className={styles.container}>
+      <Head>
+        <title>Prolog</title>
+        <meta name="description" content="Error monitoring" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <HorizontalNavigation />
-      <button
-        className={styles.contactButton}
-        onClick={() =>
-          alert(
-            "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal",
-          )
-        }
-      >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/icons/message.svg" alt="Contact" />
-      </button>
+
+      <main>
+        <div className={styles.contentContainer}>{children}</div>
+      </main>
     </div>
   );
 }
